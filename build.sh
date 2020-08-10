3872b4f12338a21a4f8e5483af76f9fc6154d80e
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-08-10 12:17:21 +0100

    Initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+mkdir target
+cd target
+curl -s --fail -L https://ratings.food.gov.uk/open-data/en-gb | grep -Eo 'http://ratings.food.gov.uk/OpenDataFiles/.*\.xml' | grep -v "cy-" | xargs -P20 -n1 -I{} curl -w "%{http_code}" -s --fail -LO {}
+cd ..
+
+grep -l FHRSEstablishment target/*.xml | grep -v cy- | xargs xsltproc extract_to_csv.xslt | grep -v '^[^0-9]' | grep -v '^$' | awk -F, '$9' > target/complete.csv
+
+gzip target/complete.csv
