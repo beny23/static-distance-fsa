f6a7421f2cdfc625f569794a653dcbcb47165604
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-08-10 12:30:14 +0100

    Better build messages

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-set -ex
+set -e
 
-mkdir target
+mkdir -p target
 cd target
-curl -s --fail -L https://ratings.food.gov.uk/open-data/en-gb | grep -Eo 'http://ratings.food.gov.uk/OpenDataFiles/.*\.xml' | grep -v "cy-" | xargs -P1 -n1 -I{} curl -w "{} %{http_code}, " -s --fail -LO {}
+curl -s --fail -L https://ratings.food.gov.uk/open-data/en-gb | grep -Eo 'http://ratings.food.gov.uk/OpenDataFiles/.*\.xml' | grep -v "cy-" | while read f; do
+  printf "$f: "
+  curl -w "%{http_code}" -s --fail -LO $f
+  echo " done"
+done
 cd ..
 
+echo "Turning into CSV"
 grep -l FHRSEstablishment target/*.xml | grep -v cy- | xargs xsltproc extract_to_csv.xslt | grep -v '^[^0-9]' | grep -v '^$' | awk -F, '$9' > target/complete.csv
+echo "Number of lines `wc -l target/complete.csv`"
 
+echo "Compressing"
 gzip target/complete.csv
+echo "Compressing done"
