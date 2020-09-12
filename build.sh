3c1af07f49070dd4ef9460e8b4d389475fe55ab4
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2020-09-13 00:09:42 +0100

    Build GeoJSON file

--- build.sh
+++ build.sh
@@ -14,6 +14,10 @@ echo "Turning into CSV"
 grep -l FHRSEstablishment target/*.xml | grep -v cy- | xargs xsltproc extract_to_csv.xslt | grep -v '^[^0-9]' | grep -v '^$' | awk -F, '$9' > target/complete.csv
 echo "Number of lines `wc -l target/complete.csv`"
 
+echo "Turning into GeoJSON"
+cat <(echo fhrsId,laBusId,name,typeId,typeName,addr1,addr2,addr3,postcode,ratingValue,ratingKey,lat,lon) target/complete.csv | node_modules/csv2geojson/csv2geojson --lat lat --lon lon | jq -c . | gzip -c > target/complete.geojson.gz
+
 echo "Compressing"
 gzip target/complete.csv
 echo "Compressing done"
+
